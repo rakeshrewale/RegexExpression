f50f370afe6d90935fe593faf28467090399c5b8
rakeshrewale <rakeshrewale993@gmail.com>
2022-07-04 23:49:10 +0530

    Added: This is valid last name

--- src/com/RegexExpression/RegexExpression.java
+++ src/com/RegexExpression/RegexExpression.java
@@ -1,19 +1,27 @@
 package com.RegexExpression;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class RegexExpression {
 
-	static String firstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+	static boolean validate(String pattern, String text) {
+		return Pattern.compile(pattern).matcher(text).matches();
+	}
 
 	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the first name :=");
-		String testName = scan.nextLine();
 
-		if (testName.matches(firstName)) {
-			System.out.println("This is valid first name .");
+		String pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please Enter Last name:=");
+
+		String lastName = scanner.next();
+
+		boolean isValid = validate(pattern, lastName);
+
+		if (isValid) {
+			System.out.println("This is valid last name .");
 		} else {
 			System.out.println("Not a valid name");
 		}
